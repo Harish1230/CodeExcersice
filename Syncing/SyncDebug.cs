81d164647de7ba769d18a29c3868db2020c6cc2a
Harish1230 <hharish270@gmail.com>
2023-11-30 12:40:21 -0600

    Update Parrel Processing

--- Syncing/SyncDebug.cs
+++ Syncing/SyncDebug.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace DeveloperSample.Syncing
@@ -12,37 +11,22 @@ namespace DeveloperSample.Syncing
         public List<string> InitializeList(IEnumerable<string> items)
         {
             var bag = new ConcurrentBag<string>();
-            Parallel.ForEach(items, async i =>
+            Parallel.ForEach(items, i =>
             {
-                var r = await Task.Run(() => i).ConfigureAwait(false);
-                bag.Add(r);
+                bag.Add(i);
             });
-            var list = bag.ToList();
-            return list;
+            return bag.ToList();
         }
 
         public Dictionary<int, string> InitializeDictionary(Func<int, string> getItem)
         {
-            var itemsToInitialize = Enumerable.Range(0, 100).ToList();
-
             var concurrentDictionary = new ConcurrentDictionary<int, string>();
-            var threads = Enumerable.Range(0, 3)
-                .Select(i => new Thread(() => {
-                    foreach (var item in itemsToInitialize)
-                    {
-                        concurrentDictionary.AddOrUpdate(item, getItem, (_, s) => s);
-                    }
-                }))
-                .ToList();
+            var itemsToInitialize = Enumerable.Range(0, 100);
 
-            foreach (var thread in threads)
+            Parallel.ForEach(itemsToInitialize, item =>
             {
-                thread.Start();
-            }
-            foreach (var thread in threads)
-            {
-                thread.Join();
-            }
+                concurrentDictionary.TryAdd(item, getItem(item));
+            });
 
             return concurrentDictionary.ToDictionary(kv => kv.Key, kv => kv.Value);
         }
